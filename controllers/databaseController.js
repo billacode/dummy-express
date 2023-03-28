ac3f97fbd6e3e1dfbe87e180b250003af723afb4
debilla <dimuthu.billa@gmail.com>
2023-03-28 16:10:32 +0530

    new db change

--- controllers/databaseController.js
+++ controllers/databaseController.js
@@ -1,40 +1,31 @@
 const { Pool } = require("pg");
 const dotenv = require("dotenv");
 dotenv.config();
- 
-let pool = null; 
+
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+    ssl: true,
+    max: 20,
+    idleTimeoutMillis: 30000
+});
 
 const connectDb = async () => {
-    try {
-        pool = new Pool({
-            user: process.env.PGUSER,
-            host: process.env.PGHOST,
-            database: process.env.PGDATABASE,
-            password: process.env.PGPASSWORD,
-            port: process.env.PGPORT,
-            ssl: true,
-        });
-
-        await pool.connect();
-    } catch (error) {
-        console.log(error)
-    }
+    
 }
 
 const disconnectDb = async () => {
-    try {
-        if (pool) await pool.end();
-        pool = null;
-    } catch (error) {
-        console.log(error.message)
-    }
+
 }
 
 const query = async (queryString, values) => {
     try {
         return await pool.query(queryString, values);
     } catch (error) {
-        console.log(error)
+        throw error;
     }
 }
 
