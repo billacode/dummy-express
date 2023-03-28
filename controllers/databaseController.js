875db0bfd23a736549d2fb490c7ea9fcd8209614
debilla <dimuthu.billa@gmail.com>
2023-03-28 11:07:05 +0530

    Database query changes

--- controllers/databaseController.js
+++ controllers/databaseController.js
@@ -2,44 +2,29 @@ const { Pool } = require("pg");
 const dotenv = require("dotenv");
 dotenv.config();
  
-let pool = null; 
-
-const connectDb = async () => {
-    try {
-        pool = new Pool({
-            user: process.env.PGUSER,
-            host: process.env.PGHOST,
-            database: process.env.PGDATABASE,
-            password: process.env.PGPASSWORD,
-            port: process.env.PGPORT,
-            ssl: true,
-        });
-
-        await pool.connect();
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-const disconnectDb = async () => {
-    try {
-        if (pool) await pool.end();
-        pool = null;
-    } catch (error) {
-        console.log(error.message)
-    }
-}
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+    ssl: true,
+    max: 2
+});
 
 const query = async (queryString, values) => {
     try {
-        return await pool.query(queryString, values);
+        const client = await pool.connect();
+        const results = await pool.query(queryString, values);
+
+        client.release();
+
+        return results;
     } catch (error) {
         console.log(error)
     }
 }
 
 module.exports = {
-    query,
-    connectDb,
-    disconnectDb
+    query
 }
--- controllers/sponsorController.js
+++ controllers/sponsorController.js
@@ -7,11 +7,9 @@ const createSponsor = async (req, res) => {
     console.log(files);
 
     try {
-        DatabaseController.connectDb();
         const response = await DatabaseController.query(`INSERT INTO sponsors (name, "contactNo", email, files) 
         VALUES($1, $2, $3, $4)`, [name, contactNo, email, fileArr]);
     
-        DatabaseController.disconnectDb();
         res.json({message: response});
     } catch (e) {
         res.json({error: e, message: e.message});
@@ -23,7 +21,6 @@ const getSponsor = async (req, res) => {
     console.log(id)
 
     try {
-        DatabaseController.connectDb();
         let response = null;
 
         if (id) {
@@ -32,7 +29,6 @@ const getSponsor = async (req, res) => {
             response = await DatabaseController.query(`select * from sponsors order by id ASC`);
         }
     
-        DatabaseController.disconnectDb();
         res.json({message: response && response.rows ? response.rows : []});
     } catch (e) {
         res.json({error: e, message: e.message});
@@ -47,11 +43,9 @@ const updateSponsor = async (req, res) => {
     console.log(files);
 
     try {
-        DatabaseController.connectDb();
         const response = await DatabaseController.query(`UPDATE sponsors SET name=$1, "contactNo"=$2, email=$3, files=$4
         WHERE id=$5`, [name, contactNo, email, fileArr, id]);
     
-        DatabaseController.disconnectDb();
         res.send({message: response});
     } catch (e) {
         res.json({error: e, message: e.message});
@@ -62,10 +56,8 @@ const deleteSponsor = async (req, res) => {
     const { id } = req.query;
 
     try {
-        DatabaseController.connectDb();
         const response = await DatabaseController.query(`DELETE FROM sponsors WHERE id=$1`, [id]);
     
-        DatabaseController.disconnectDb();
         res.send({message: response});
     } catch (e) {
         res.json({error: e, message: e.message});
